62d8f6aaff3c33bcf254bd6a0622df76d0ab696b
Alande Amorim <alande.amorim@gmail.com>
2025-03-19 10:48:40 -0300

    feat: add authentication pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router";
+import SignIn from "./sign-in/SignIn";
+import SignUp from "./sign-up/Signup";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route index path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
+  </BrowserRouter>
 );
